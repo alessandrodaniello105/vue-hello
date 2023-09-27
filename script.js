85058558f2b4a8495809d64c1c3a6070a0595188
alessandrodaniello105 <kanymsevement@gmail.com>
2023-09-27 13:29:43 +0200

    [add]: print greetings in DOM

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 // 3. Inizializzo un'istanza di createApp di Vue, che monto selezionando l'#app (fra apici)
 const {createApp} = Vue;
+
+// 4. Vado a definire i dati all'interno del mio data() che mi restituisce un oggetto
+
+createApp({
+  data(){
+    return {
+      greetings: 'Hello World!'
+    }
+  }
+}).mount('#app');
